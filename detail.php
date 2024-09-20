73d9cdd91039b7b275b711dafdef1983cb3d2ba4
Cody King <122841869+Cjking57893@users.noreply.github.com>
2024-09-19 20:39:36 -0400

    Finished up languages section

--- detail.php
+++ detail.php
@@ -418,6 +418,7 @@
 	// Storing array data for selected team member in variables.
 	$data = $team_members[$id];
 	$education = $data['education'];
+	$languages = $data['languages'];
 	
 ?>
 <!DOCTYPE html>
@@ -699,9 +700,12 @@
 						    <h2 class="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Languages</h2>
 						    <div class="resume-section-content">
 							    <ul class="list-unstyled resume-lang-list">
-								    <li class="mb-2"><span class="resume-lang-name font-weight-bold">English</span> <small class="text-muted font-weight-normal">(Native)</small></li>
-								    <li class="mb-2 align-middle"><span class="resume-lang-name font-weight-bold">French</span> <small class="text-muted font-weight-normal">(Professional)</small></li>
-								    <li><span class="resume-lang-name font-weight-bold">Spanish</span> <small class="text-muted font-weight-normal">(Professional)</small></li>
+									<?php
+										foreach($languages as $language) {
+											echo
+											'<li class="mb-2 align-middle"><span class="resume-lang-name font-weight-bold">' . $language . '</span>';
+										}
+									?>
 							    </ul>
 						    </div>
 					    </section><!--//language-section-->
